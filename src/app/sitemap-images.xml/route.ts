72cad6383776f1b985c1885bdf2f91f365c79837
Sanzid <mdsanjidahmed3@gmail.com>
2026-06-05 02:00:30 +0600

    Make all XML routes fully dynamic

--- src/app/sitemap-images.xml/route.ts
+++ src/app/sitemap-images.xml/route.ts
@@ -2,6 +2,8 @@ import connectToDatabase from '@/lib/mongodb/mongoose';
 import Phone from '@/lib/models/Phone';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   await connectToDatabase();
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://techtweak.com';
