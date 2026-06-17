472553c8546af455264f87bb03b35835aeaa36cf
Sanzid <mdsanjidahmed3@gmail.com>
2026-06-18 00:32:55 +0600

    Security: Scrub remaining root credentials

--- get_phone.mjs
+++ get_phone.mjs
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 import { MongoClient } from 'mongodb';
 
-const uri = "mongodb://sanzid_admin:sanzid4%40@ac-olfahzz-shard-00-00.fhnlrss.mongodb.net:27017,ac-olfahzz-shard-00-01.fhnlrss.mongodb.net:27017,ac-olfahzz-shard-00-02.fhnlrss.mongodb.net:27017/techtweak?ssl=true&authSource=admin&replicaSet=atlas-109zzp-shard-0&appName=TechTweak";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 async function run() {
--- get_vivo_id.js
+++ get_vivo_id.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 const { MongoClient } = require('mongodb');
-const uri = "mongodb://sanzid_admin:sanzid4%40@ac-olfahzz-shard-00-00.fhnlrss.mongodb.net:27017,ac-olfahzz-shard-00-01.fhnlrss.mongodb.net:27017,ac-olfahzz-shard-00-02.fhnlrss.mongodb.net:27017/techtweak?ssl=true&authSource=admin&replicaSet=atlas-109zzp-shard-0&appName=TechTweak";
+const uri = process.env.MONGODB_URI;
 
 async function run() {
   const client = new MongoClient(uri);
